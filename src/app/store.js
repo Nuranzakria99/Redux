2727d5e74cbaf794aeb1fa0c13fd8e3e31305bd1
nuranzakria99 <nuranzakria99@gmail.com>
2024-11-01 08:19:18 +0200

    register services in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import {productsApi} from './service/dummyData'
 
 configureStore({
     reducer:{
-        
+      [productsApi.reducerPath] : productsApi.reducer
+    },
+
+
+    //It using for Cashing
+    middleware: (getDefaultMiddleware) => {
+        getDefaultMiddleware().concat(productsApi.middleware)
     }
 })
+
+// eslint-disable-next-line no-undef
+setupListeners(store.dispatch)
